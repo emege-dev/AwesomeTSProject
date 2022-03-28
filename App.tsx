21ffaf6c1831a55facabd4d59129f10af2fb4793
emege-dev <enrique.melida@gmail.com>
2022-03-28 10:30:25 -0400

    -

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const App = ( )=> 
+{
+  return (
+          <View>
+           <Text>Hola Mundo!!!!! REACT-NATIVE!!! la concha de la lorraaaaaaaaa</Text> 
+          </View>
+  );
+};
+
+export default App;
